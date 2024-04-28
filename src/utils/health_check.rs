f5b48889a821088af1f819ae1a8770e4cee3d7c1
saar120 <saar120@gmail.com>
2024-04-28 18:35:37 +0300

    health check for koyeb

--- src/utils/health_check.rs
+++ src/utils/health_check.rs
@@ -4,17 +4,25 @@ use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
 pub async fn start_tcp_server(addr: &str) -> Result<(), Box<dyn std::error::Error>> {
     let listener = TcpListener::bind(addr).await?;
-
     info!("Server running on {}", addr);
 
     loop {
-        let (mut socket, _) = listener.accept().await?;
-        tokio::spawn(async move {
-            let mut buf = [0; 1024];
-            // Handle each connection in a separate task.
-            while let Ok(n) = socket.read(&mut buf).await {
-                if n == 0 { break; } // Connection was closed
+        match listener.accept().await {
+            Ok((mut socket, _)) => {
+                tokio::spawn(async move {
+                    let mut buf = [0; 1024];
+                    while let Ok(n) = socket.read(&mut buf).await {
+                        if n == 0 { break; } // Connection was closed
+                    }
+                });
+                info!("Health check executed: Connection accepted");
+            },
+            Err(e) => {
+                error!("Failed to accept connection: {}", e);
+                break;
             }
-        });
+        }
     }
+
+    Ok(())
 }
